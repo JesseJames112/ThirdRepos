bad911bbcfdc50603a00f4a32f93e49077a83659
jesse <123@123.com>
2018-03-05 15:23:45 +1030

    commit the back stall ticket changes

--- MyInitial/TicketsForm.Designer.cs
+++ MyInitial/TicketsForm.Designer.cs
@@ -38,6 +38,7 @@
             this.radBox = new System.Windows.Forms.RadioButton();
             this.grpSection = new System.Windows.Forms.GroupBox();
             this.cmdCalculate = new System.Windows.Forms.Button();
+            this.backStallButton = new System.Windows.Forms.RadioButton();
             this.grpSection.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -118,6 +119,7 @@
             // 
             // grpSection
             // 
+            this.grpSection.Controls.Add(this.backStallButton);
             this.grpSection.Controls.Add(this.radBox);
             this.grpSection.Controls.Add(this.radGeneral);
             this.grpSection.Controls.Add(this.radBalcony);
@@ -137,6 +139,17 @@
             this.cmdCalculate.Text = "Calculate";
             this.cmdCalculate.UseVisualStyleBackColor = true;
             this.cmdCalculate.Click += new System.EventHandler(this.cmdCalculate_Click);
+            // 
+            // backStallButton
+            // 
+            this.backStallButton.AutoSize = true;
+            this.backStallButton.Location = new System.Drawing.Point(26, 86);
+            this.backStallButton.Name = "backStallButton";
+            this.backStallButton.Size = new System.Drawing.Size(73, 17);
+            this.backStallButton.TabIndex = 11;
+            this.backStallButton.TabStop = true;
+            this.backStallButton.Text = "Back Stall";
+            this.backStallButton.UseVisualStyleBackColor = true;
             // 
             // TicketsForm
             // 
@@ -172,6 +185,7 @@
         private System.Windows.Forms.RadioButton radBox;
         private System.Windows.Forms.GroupBox grpSection;
         private System.Windows.Forms.Button cmdCalculate;
+        private System.Windows.Forms.RadioButton backStallButton;
     }
 }
 
